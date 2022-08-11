e36744eacfa0517bc04bcd6abba28e689a3192b6
Himanshu Rathod <rathodhimanshu609@gmail.com>
2022-08-11 10:09:09 +0530

    hello 2nd commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,12 @@
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMfHUVXe4GDHryY7sJMPmnElS5kLDZZo4",
-  authDomain: "crypto-hunter-50991.firebaseapp.com",
-  projectId: "crypto-hunter-50991",
-  storageBucket: "crypto-hunter-50991.appspot.com",
-  messagingSenderId: "141181984273",
-  appId: "1:141181984273:web:ba51a25e27ce6a2b1d9bb0",
+  apiKey: "AIzaSyBmtEkhXWkq2AitNwd4MFiRGFZV9aCuZ84",
+  authDomain: "crypto-hunter-fd019.firebaseapp.com",
+  projectId: "crypto-hunter-fd019",
+  storageBucket: "crypto-hunter-fd019.appspot.com",
+  messagingSenderId: "939666208118",
+  appId: "1:939666208118:web:6eb2f9fa1ef9d09d5fb657"
 };
 
 export default firebaseConfig;
